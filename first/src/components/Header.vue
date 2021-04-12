ebf200d12fafa2e4d955f9f61c09741a0710d84e
Veronika Sampetova <81315475+sampetova@users.noreply.github.com>
2021-04-13 00:10:21 +0300

    build config

---
+++ first/src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="header"><h1>Я хедер</h1></header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
--- first/vue.config.js
+++ first/vue.config.js
@@ -1,16 +1,17 @@
 module.exports = {
-  devServer: {
-    overlay: {
-      warnings: false,
-      errors: true
+    devServer: {
+        overlay: {
+            warnings: false,
+            errors: true
+        }
+    },
+    chainWebpack: config => {
+        config.module
+            .rule("eslint")
+            .use("eslint-loader")
+            .options({
+                fix: true
+            });
+        config.optimization.minimize(true);
     }
-  },
-  chainWebpack: config => {
-    config.module
-      .rule("eslint")
-      .use("eslint-loader")
-      .options({
-        fix: true
-      });
-  }
 };
