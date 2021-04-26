3ad400682a173f6cab811af3d1b7276f6d968ffa
Veronika Sampetova <81315475+sampetova@users.noreply.github.com>
2021-04-26 18:01:41 +0300

    change path

--- first/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import App from "./App";
-import router from "@/router/index";
-import "@/assets/styles/style.scss";
-const app = createApp(App);
-app.use(router);
-app.mount("#app");
